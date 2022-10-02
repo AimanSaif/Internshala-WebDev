ffb3093408a1f778602bd3e93456a951606ae5da
aimansaif <asaif@ualberta.ca>
2022-10-02 12:01:17 -0600

    Web Developmengt Training

---
+++ Training-Assignment/SQL-Assignment 3/insert.sql
@@ -0,0 +1,14 @@
+INSERT INTO items(id, name, price)
+VALUES
+    (1, 'Canon EOS',36000),
+    (2, 'Nikon DSLR',40000),
+    (3, 'SONY PIXAR', 37000),
+    (4, 'BRAVIA', 65000),
+    (5, 'iPHONE', 45000),
+    (6, 'SAMSUNG GALAXY', 40000),
+    (7, 'EARPHONES, 450),
+    (8, 'LAPTOP', 46000),
+    (9, 'MOUSE', 800),
+    (10, 'KEYBOARD', 1000),
+    (11, 'Toy Car', 700),
+    (12, 'Toy Phone', 500);
